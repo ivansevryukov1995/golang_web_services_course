d43c31c4b9eae3778d20535a856fd9f846ea2e81
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-11-25 22:27:21 +0300

    started implementing the dirTree function

--- 1/main.go
+++ 1/main.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"log"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
+func dirTree(out *os.File, path string, printFiles bool) error {
+
+	dir, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+
+	f, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, file := range f {
+		if i == len(f)-1 {
+			fmt.Printf("└───%s\n", file.Name())
+			if file.IsDir() {
+				fmt.Printf("\t")
+				return dirTree(out, path+string(os.PathSeparator)+file.Name(), printFiles)
+			}
+		} else {
+			fmt.Printf("├───%s\n", file.Name())
+			if file.IsDir() {
+				fmt.Printf("\t")
+				return dirTree(out, path+string(os.PathSeparator)+file.Name(), printFiles)
+			}
+		}
+
+	}
+
+	return nil
+}
+
 func main() {
+
 	out := os.Stdout
 	if !(len(os.Args) == 2 || len(os.Args) == 3) {
 		panic("usage go run main.go . [-f]")
@@ -19,4 +51,5 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
 }

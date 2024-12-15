3f0c387c810ceecaabb8a0485053a110c8827946
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-12-15 14:13:18 +0300

    Final code second task

--- 2/signer.go
+++ 2/signer.go
@@ -10,47 +10,46 @@ import (
 )
 
 func SingleHash(in, out chan interface{}) {
-	wg := &sync.WaitGroup{}
+	leftValueCh := make(chan string, 1)
+	rightValueCh := make(chan string, 1)
 
 	for ch := range in {
-		wg.Add(1)
 		md5 := DataSignerMd5(fmt.Sprintf("%v", ch))
 
-		go func(out chan interface{}, wg *sync.WaitGroup) {
+		go func(data string, leftCh chan string) { // Параллелим левую и правую часть
+			leftCh <- DataSignerCrc32(data)
+		}(fmt.Sprintf("%v", ch), leftValueCh)
+
+		go func(data string, rightCh chan string) {
+			rightCh <- DataSignerCrc32(md5)
+		}(md5, rightValueCh)
+
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		go func(out chan interface{}, leftCh chan string, rightCh chan string, wg *sync.WaitGroup) {
 			defer wg.Done()
-
-			rightValue := DataSignerCrc32(md5)
-
-			leftValueCh := make(chan string, 1)
-			go func(ch interface{}, leftValueCh chan string) {
-				leftValueCh <- DataSignerCrc32(fmt.Sprintf("%v", ch))
-			}(ch, leftValueCh)
-			leftValue := <-leftValueCh
-
+			leftValue := <-leftCh
+			rightValue := <-rightCh
 			out <- leftValue + "~" + rightValue
-		}(out, wg)
+		}(out, leftValueCh, rightValueCh, wg)
 		defer wg.Wait()
 	}
 }
 
 func MultiHash(in, out chan interface{}) {
-
 	for ch := range in {
 		wg := &sync.WaitGroup{}
-		counter := make(chan interface{})
+		counter := make(chan string)
 		var counters = map[int]string{}
 
-		wg.Add(1)
-		go func(counter chan interface{}, wg *sync.WaitGroup) {
-			defer wg.Done()
-			for th := 0; th < 6; th++ {
-				wg.Add(1)
-				go func(counter chan interface{}, wg *sync.WaitGroup) {
-					defer wg.Done()
-					counter <- fmt.Sprintf("%d_", th) + DataSignerCrc32(fmt.Sprintf("%v", th)+fmt.Sprintf("%v", ch))
-				}(counter, wg)
-			}
-		}(counter, wg)
+		for th := 0; th < 6; th++ {
+			wg.Add(1)
+			go func(counter chan string, wg *sync.WaitGroup) {
+				defer wg.Done()
+				counter <- fmt.Sprintf("%d_", th) + DataSignerCrc32(fmt.Sprintf("%v", th)+fmt.Sprintf("%v", ch))
+			}(counter, wg)
+		}
+
 		go func() {
 			wg.Wait()
 			close(counter)
@@ -58,16 +57,14 @@ func MultiHash(in, out chan interface{}) {
 
 		wg1 := &sync.WaitGroup{}
 		wg1.Add(1)
-		go func(out chan interface{}, wg1 *sync.WaitGroup) {
-			defer wg1.Done()
+		go func(out chan interface{}, wg *sync.WaitGroup) {
+			defer wg.Done()
 			for count := range counter {
-				i, err := strconv.Atoi(strings.Split(count.(string), "_")[0])
+				i, err := strconv.Atoi(strings.Split(count, "_")[0])
 				if err != nil {
 					log.Fatal(err)
 				}
-				// mu.Lock()
-				counters[i] = strings.Split(count.(string), "_")[1]
-				// mu.Unlock()
+				counters[i] = strings.Split(count, "_")[1]
 			}
 
 			keys := make([]int, 0, len(counters))
@@ -86,7 +83,6 @@ func MultiHash(in, out chan interface{}) {
 
 		}(out, wg1)
 		defer wg1.Wait()
-
 	}
 }
 
@@ -100,7 +96,6 @@ func CombineResults(in, out chan interface{}) {
 	sort.Strings(hashResults)
 
 	out <- strings.Join(hashResults, "_")
-
 }
 
 func ExecutePipeline(hashSignJobs ...job) {
@@ -121,3 +116,16 @@ func ExecutePipeline(hashSignJobs ...job) {
 		defer wg.Wait()
 	}
 }
+
+// Код пока не самый лучший, потом может исправлю :)
+// === RUN   TestByIlia
+// collected 3
+// collected 9
+// collected 12
+// --- PASS: TestByIlia (0.30s)
+// === RUN   TestPipeline
+// --- PASS: TestPipeline (0.01s)
+// === RUN   TestSigner
+// --- PASS: TestSigner (2.07s)
+// PASS
+// ok      2       2.565s

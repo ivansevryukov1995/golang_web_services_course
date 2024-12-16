1315ad631be57a0f2344ca55c6fb9045a2099e8d
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-12-16 17:12:49 +0300

    using the struct

--- 2/signer.go
+++ 2/signer.go
@@ -10,6 +10,8 @@ import (
 const Th = 6
 
 func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+
 	leftValueCh := make(chan string, 1)
 	rightValueCh := make(chan string, 1)
 
@@ -26,67 +28,56 @@ func SingleHash(in, out chan interface{}) {
 			rightCh <- DataSignerCrc32(md5)
 		}(md5, rightValueCh)
 
-		wg := &sync.WaitGroup{}
 		wg.Add(1)
 		go func(out chan interface{}, leftCh chan string, rightCh chan string, wg *sync.WaitGroup) {
 			defer wg.Done()
 			out <- <-leftCh + "~" + <-rightCh
 		}(out, leftValueCh, rightValueCh, wg)
-		defer wg.Wait()
+
 	}
+	defer wg.Wait()
+}
+
+type ThCh struct {
+	ind  int
+	data string
 }
 
 func MultiHash(in, out chan interface{}) {
+
+	wg := &sync.WaitGroup{}
+
+	thCh0 := make(chan string, 1)
+	thCh1 := make(chan string, 1)
+	thCh2 := make(chan string, 1)
+	thCh3 := make(chan string, 1)
+	thCh4 := make(chan string, 1)
+	thCh5 := make(chan string, 1)
+
+	chanel := map[string]chan string{
+		"thCh0": thCh0,
+		"thCh1": thCh1,
+		"thCh2": thCh2,
+		"thCh3": thCh3,
+		"thCh4": thCh4,
+		"thCh5": thCh5,
+	}
+
 	for ch := range in {
-		counter := make(chan string, Th)
-		ind := make(chan int, Th)
-
-		wg := &sync.WaitGroup{}
-
-		//Записывам в канал counter значения crc32(th+data),
-		//а в канал ind значения th
 		for th := 0; th < Th; th++ {
-			wg.Add(1)
-			go func(counter chan string, wg *sync.WaitGroup) {
-				defer wg.Done()
-				counter <- DataSignerCrc32(fmt.Sprintf("%v", th) + fmt.Sprintf("%v", ch))
-				ind <- th
-			}(counter, wg)
+			go func(th string, ch string, thCh chan string) {
+				thCh <- DataSignerCrc32(th + ch)
+			}(fmt.Sprintf("%v", th), fmt.Sprintf("%v", ch), chanel["thCh"+fmt.Sprintf("%v", th)])
 		}
 
-		go func() {
-			wg.Wait()
-			close(counter)
-			close(ind)
-		}()
+		wg.Add(1)
+		go func(out chan interface{}, wg *sync.WaitGroup) {
+			defer wg.Done()
+			out <- <-thCh0 + <-thCh1 + <-thCh2 + <-thCh3 + <-thCh4 + <-thCh5
+		}(out, wg)
 
-		wg1 := &sync.WaitGroup{}
-
-		//Сортируем значения crc32(th+data) по th и пишем результат канал out
-		wg1.Add(1)
-		go func(wg1 *sync.WaitGroup) {
-			defer wg1.Done()
-
-			counters := make(map[int]string)
-			for ch := range counter {
-				counters[<-ind] = ch
-			}
-
-			keys := make([]int, 0, Th)
-			for k := range counters {
-				keys = append(keys, k)
-			}
-			sort.Ints(keys)
-
-			var result string
-			for _, val := range keys {
-				result = result + counters[val]
-			}
-
-			out <- result
-		}(wg1)
-		defer wg1.Wait()
 	}
+	defer wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -118,29 +109,3 @@ func ExecutePipeline(hashSignJobs ...job) {
 		defer wg.Wait()
 	}
 }
-
-// func MultiHash(in, out chan interface{}) {
-// 	th0Ch := make(chan string, 1)
-// 	th1Ch := make(chan string, 1)
-// 	th2Ch := make(chan string, 1)
-// 	th3Ch := make(chan string, 1)
-// 	th4Ch := make(chan string, 1)
-// 	th5Ch := make(chan string, 1)
-
-// 	for ch := range in {
-// 		go func(th0Ch chan string) { th0Ch <- DataSignerCrc32(fmt.Sprintf("%v", 0) + fmt.Sprintf("%v", ch)) }(th0Ch)
-// 		go func(th1Ch chan string) { th1Ch <- DataSignerCrc32(fmt.Sprintf("%v", 1) + fmt.Sprintf("%v", ch)) }(th1Ch)
-// 		go func(th2Ch chan string) { th2Ch <- DataSignerCrc32(fmt.Sprintf("%v", 2) + fmt.Sprintf("%v", ch)) }(th2Ch)
-// 		go func(th3Ch chan string) { th3Ch <- DataSignerCrc32(fmt.Sprintf("%v", 3) + fmt.Sprintf("%v", ch)) }(th3Ch)
-// 		go func(th4Ch chan string) { th4Ch <- DataSignerCrc32(fmt.Sprintf("%v", 4) + fmt.Sprintf("%v", ch)) }(th4Ch)
-// 		go func(th5Ch chan string) { th5Ch <- DataSignerCrc32(fmt.Sprintf("%v", 5) + fmt.Sprintf("%v", ch)) }(th5Ch)
-
-// 		wg := &sync.WaitGroup{}
-// 		wg.Add(1)
-// 		go func(out chan interface{}, wg *sync.WaitGroup) {
-// 			defer wg.Done()
-// 			out <- <-th0Ch + <-th1Ch + <-th2Ch + <-th3Ch + <-th4Ch + <-th5Ch
-// 		}(out, wg)
-// 		defer wg.Wait()
-// 	}
-// }

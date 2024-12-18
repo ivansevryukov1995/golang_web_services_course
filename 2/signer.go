c831a28ab9f2d898e28ffc2c4502e70f392aee07
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-12-18 13:11:34 +0300

    Add comments

--- 2/signer.go
+++ 2/signer.go
@@ -12,29 +12,29 @@ const Th = 6
 func SingleHash(in, out chan interface{}) {
 	wg := &sync.WaitGroup{}
 
-	leftValueCh := make(chan string, 1)
-	rightValueCh := make(chan string, 1)
+	chanLeft := make(chan string, 1)
+	chanRight := make(chan string, 1)
 
 	for ch := range in {
 		// DataSignerMd5 Считаем вне горутин, чтобы не перегреваться
 		// Параллелим левую и правую часть, так как DataSignerCrc32, считается 1 сек
 		md5 := DataSignerMd5(fmt.Sprintf("%v", ch))
 
-		go func(data string, leftCh chan string) {
-			leftCh <- DataSignerCrc32(data)
-		}(fmt.Sprintf("%v", ch), leftValueCh)
+		go func(data string, chanLeft chan string) {
+			chanLeft <- DataSignerCrc32(data)
+		}(fmt.Sprintf("%v", ch), chanLeft)
 
-		go func(data string, rightCh chan string) {
-			rightCh <- DataSignerCrc32(md5)
-		}(md5, rightValueCh)
+		go func(data string, chanRight chan string) {
+			chanRight <- DataSignerCrc32(data)
+		}(md5, chanRight)
 
 		wg.Add(1)
-		go func(out chan interface{}, leftCh chan string, rightCh chan string, wg *sync.WaitGroup) {
+		go func(out chan interface{}, chanLeft chan string, chanRight chan string, wg *sync.WaitGroup) {
 			defer wg.Done()
-			out <- <-leftCh + "~" + <-rightCh
-		}(out, leftValueCh, rightValueCh, wg)
-
+			out <- <-chanLeft + "~" + <-chanRight
+		}(out, chanLeft, chanRight, wg)
 	}
+
 	defer wg.Wait()
 }
 
@@ -42,36 +42,39 @@ func MultiHash(in, out chan interface{}) {
 
 	wg := &sync.WaitGroup{}
 
-	thCh0 := make(chan string, 1)
-	thCh1 := make(chan string, 1)
-	thCh2 := make(chan string, 1)
-	thCh3 := make(chan string, 1)
-	thCh4 := make(chan string, 1)
-	thCh5 := make(chan string, 1)
+	// Создаем 6 каналов, в которые отправим результаты DataSignerCrc32
+	// чтобы значение th соответствовало имени канала и не приходилось использовать непотокобезобастные типы данных
 
-	chanel := map[string]chan string{
-		"thCh0": thCh0,
-		"thCh1": thCh1,
-		"thCh2": thCh2,
-		"thCh3": thCh3,
-		"thCh4": thCh4,
-		"thCh5": thCh5,
+	chanTh0 := make(chan string, 1)
+	chanTh1 := make(chan string, 1)
+	chanTh2 := make(chan string, 1)
+	chanTh3 := make(chan string, 1)
+	chanTh4 := make(chan string, 1)
+	chanTh5 := make(chan string, 1)
+
+	chanels := map[string]chan string{
+		"chanTh0": chanTh0,
+		"chanTh1": chanTh1,
+		"chanTh2": chanTh2,
+		"chanTh3": chanTh3,
+		"chanTh4": chanTh4,
+		"chanTh5": chanTh5,
 	}
 
 	for ch := range in {
 		for th := 0; th < Th; th++ {
-			go func(th string, ch string, thCh chan string) {
-				thCh <- DataSignerCrc32(th + ch)
-			}(fmt.Sprintf("%v", th), fmt.Sprintf("%v", ch), chanel["thCh"+fmt.Sprintf("%v", th)])
+			go func(th string, data string, chanTh chan string) {
+				chanTh <- DataSignerCrc32(th + data)
+			}(fmt.Sprintf("%v", th), fmt.Sprintf("%v", ch), chanels["chanTh"+fmt.Sprintf("%v", th)])
 		}
 
 		wg.Add(1)
 		go func(out chan interface{}, wg *sync.WaitGroup) {
 			defer wg.Done()
-			out <- <-thCh0 + <-thCh1 + <-thCh2 + <-thCh3 + <-thCh4 + <-thCh5
+			out <- <-chanTh0 + <-chanTh1 + <-chanTh2 + <-chanTh3 + <-chanTh4 + <-chanTh5
 		}(out, wg)
-
 	}
+
 	defer wg.Wait()
 }
 
